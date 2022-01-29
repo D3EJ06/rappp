04e2c4fc2b0a70c062740eaec51f3b3c48d10568
D3EJ06 <98523363+D3EJ06@users.noreply.github.com>
2022-01-29 17:41:08 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -249,13 +249,13 @@ function App() {
             >
               <StyledButton
                 onClick={(e) => {
-                  window.open("/config/roadmap.pdf", "_blank");
+                  window.open("https://twitter.com/D3ej_Nft", "_blank");
                 }}
                 style={{
                   margin: "5px",
                 }}
               >
-                Roadmap
+                Twitter
               </StyledButton>
               <StyledButton
                 style={{
